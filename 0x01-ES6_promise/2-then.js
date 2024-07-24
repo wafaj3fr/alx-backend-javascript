4a7a1a5753b57d15c4ee0415d36f8613336aae2a
wafa <wafaagaafar@gmail.com>
2024-07-24 16:02:17 -0700

    9

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
+
+export default handleResponseFromAPI;
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const { body } = results[0];
+      const { firstName, lastName } = results[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
