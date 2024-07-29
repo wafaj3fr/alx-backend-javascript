7e2bd0205522dc2f4367b60de322c073c1d66524
wafaj3fr <wafaagaafar@gmail.com>
2024-07-29 05:07:38 +0000

    arrow

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,8 +2,5 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     const self = this;
-    this.addNeighborhood = newNeighborhood => {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
+    this.addNeighborhood = newNeighborhood => self.sanFranciscoNeighborhoods.push(newNeighborhood);
   }
