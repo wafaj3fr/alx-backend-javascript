2dda7db65e7bc4f9303a3358974597ade3275716
wafa <wafaagaafar@gmail.com>
2024-11-28 12:55:11 +0000

    chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when dividing by 0', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
