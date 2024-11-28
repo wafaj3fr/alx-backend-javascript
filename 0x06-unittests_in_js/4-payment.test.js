df360b5ec1fc41ccfb39b757a5420a672c845227
wafa <wafaagaafar@gmail.com>
2024-11-28 13:04:21 +0000

    stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with SUM, 100, 20 and log the correct message', function() {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    stub.restore();
+    consoleSpy.restore();
+  });
+});
