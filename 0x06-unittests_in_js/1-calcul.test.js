92ed6b0f2e15b885c30f4e7339ef2a1ffa7588f1
wafa <wafaagaafar@gmail.com>
2024-11-28 12:42:59 +0000

    2

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" when dividing by 0', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
