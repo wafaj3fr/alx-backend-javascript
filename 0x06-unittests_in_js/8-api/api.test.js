358e8246aa03878afc228b6e9955bdf77cb8d5b3
wafa <wafaagaafar@gmail.com>
2024-11-28 13:24:29 +0000

    Regex integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -18,3 +18,22 @@ describe('Index page', () => {
     });
   });
 });
+
+describe('Cart page', () => {
+  const baseUrl = 'http://localhost:7865/cart';
+
+  it('should return status code 200 when :id is a number', (done) => {
+    request(`${baseUrl}/12`, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is NOT a number', (done) => {
+    request(`${baseUrl}/hello`, (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
